e1ef46e75a6fadb739a50eb4655828d6d6111dee
Trevor Helms <thelms501@gmail.com>
2016-01-27 23:41:36 -0500

    Add a method to get the actual delivery time of a type of wood

--- src/model/WoodItem.java
+++ src/model/WoodItem.java
@@ -14,6 +14,7 @@ public class WoodItem {
 	String type;
 	Double baseDeliveryTime;
 	Double price;
+	Double realizedDeliveryTime;
 	
 	/**
 	 * Default constructor 
@@ -22,6 +23,7 @@ public class WoodItem {
 		this.type = "Empty";
 		this.baseDeliveryTime = 0.0;
 		this.price = 0.0;
+		this.realizedDeliveryTime = 0.0;
 	}
 	
 	/**
@@ -34,6 +36,44 @@ public class WoodItem {
 		this.type = type;
 		this.baseDeliveryTime = baseDeliveryTime;
 		this.price = price;
+		this.realizedDeliveryTime = baseDeliveryTime;
+	}
+
+	/**
+	 * Returns the realized (actual) delivery time after mulitplying by the modifier
+	 *
+	 * @param qty BF of the wood being ordered
+	 * @return Realized delivery time
+	 */
+	public Double getRealizedDeliveryTime(int qty) throws Exception {
+		double modifier = 0;
+
+		if (qty <= 0) {
+			throw new Exception("Length of wood cannot be 0");
+		}
+		else if (qty > 0 && qty <= 100) {
+			modifier = 1;
+		}
+		else if (qty > 100 && qty <= 200) {
+			modifier = 2;
+		}
+		else if (qty > 200 && qty <= 300) {
+			modifier = 3;
+		}
+		else if (qty > 300 && qty <= 400) {
+			modifier = 4;
+		}
+		else if (qty > 400 && qty <= 500) {
+			modifier = 5;
+		}
+		else if (qty > 500 && qty <= 1000) {
+			modifier = 5.5;
+		}
+		else {
+			throw new Exception("Cannot order more than 1000 BF of any one type of wood.");
+		}
+
+		return modifier * baseDeliveryTime;
 	}
 
 	/**
